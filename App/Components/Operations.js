d48cff62a4a68979dbdb141e8ff1417eea109a59
Eduardo Bernal Galan <jblalo78@gmail.com>
2018-08-21 10:30:36 -0700

    Implements operator and new state dropdown selects

--- App/Components/Operations.js
+++ App/Components/Operations.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
-import { View, Text, FlatList, TouchableHighlight } from 'react-native'
+import { View, FlatList, TouchableHighlight } from 'react-native'
 import styles from './Styles/OperationsStyle'
-import { CheckBox, Picker } from 'native-base'
+import { Picker, Button } from 'native-base'
+import { Text } from 'native-base'
 import WorkOrderActions from '../Redux/WorkOrderRedux'
 
 class Operations extends Component {
@@ -12,8 +13,11 @@ class Operations extends Component {
   }
 
   componentDidMount = () => {
-    const { requestOperators, token } = this.props
+    const { requestOperators, token, requestWooperationlog, search } = this.props
+    const { WOKey, RCTKey, OperationKey } = search
+
     requestOperators(token)
+    requestWooperationlog(token, WOKey, RCTKey, OperationKey)
   }
 
   _keyExtractor = (item, index) => item.id
@@ -34,27 +38,42 @@ class Operations extends Component {
 
   _renderItem = ({ item }) => {
     const { operators } = this.props
-    console.log({ operators })
     return (
       <TouchableHighlight>
         <View style={styles.row}>
           <Text style={styles.serial}>{item.PartPO.PartId}</Text>
-          <Text style={styles.operation}>{item.PartPO.SerialNum} X</Text>
-          <View style={styles.status}>
-            <Picker
-              mode="dropdown"
-              style={{ width: 120 }}
-              selectedValue={this.state.picker}
-              placeholder="Seleccionar Operador"
-              placeholderStyle={{ color: "#bfc6ea" }}
-              placeholderIconColor="#007aff"
-            // onValueChange={this.onValueChange.bind(this)}
-            >
-              {operators && operators.map((operator, index) => (
-                <Picker.Item label={operator.Name} value={'x'} />
-              ))}
-            </Picker>
-          </View>
+          <Text style={styles.operation}>{item.PartPO.SerialNum}</Text>
+          <Picker
+            mode="dropdown"
+            selectedValue={this.state.picker}
+            placeholder="Seleccionar Operador"
+            placeholderStyle={{ color: "#bfc6ea" }}
+            placeholderIconColor="#007aff"
+          // onValueChange={this.onValueChange.bind(this)}
+          >
+            {operators &&
+              operators.map((operator, index) => (
+                <Picker.Item label={operator.Name} value={operator.Id} />
+              ))
+            }
+          </Picker>
+          <Picker
+            mode="dropdown"
+            selectedValue={this.state.picker}
+            placeholder="Estado"
+            placeholderStyle={{ color: "#bfc6ea" }}
+            placeholderIconColor="#007aff"
+          >
+            <Picker.Item label={"Estado"} value={'Comenzar'} />
+            <Picker.Item label={"Comenzar"} value={'Comenzar'} />
+            <Picker.Item label={"Pausar"} value={'Pausar'} />
+            <Picker.Item label={"Resumir"} value={'Resumir'} />
+            <Picker.Item label={"Aprovar"} value={'Aprovar'} />
+            <Picker.Item label={"Fallo"} value={'Fallo'} />
+          </Picker>
+          <Button success rounded small>
+            <Text>Guardar</Text>
+          </Button>
         </View>
       </TouchableHighlight>
     )
@@ -70,6 +89,11 @@ class Operations extends Component {
 
   render() {
     const { operations = [] } = this.state
+
+    const { Wooperationlog } = this.props
+
+    console.log({ Wooperationlog })
+
     return (
       <View style={styles.container}>
         <FlatList
@@ -85,6 +109,7 @@ class Operations extends Component {
 
 const mapStateToProps = (state) => {
   return {
+    Wooperationlog: state.workOrder.Wooperationlog,
     operators: state.workOrder.operators,
     token: state.login.token,
     user: state.login.user
@@ -93,7 +118,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    requestOperators: token => dispatch(WorkOrderActions.operatorsRequest(token))
+    requestOperators: token => dispatch(WorkOrderActions.operatorsRequest(token)),
+    requestWooperationlog: (token, WOKey, RCTKey, OperationKey) => dispatch(WorkOrderActions.searchWooperationlogRequest(token, WOKey, RCTKey, OperationKey))
   }
 }
 
