913bd693d5374949d552c37d3e2970a3f0b2c92b
Eduardo Bernal Galan <jblalo78@gmail.com>
2018-10-24 16:43:04 -0700

    Removed console log

--- App/Sagas/LoginSagas.js
+++ App/Sagas/LoginSagas.js
@@ -4,11 +4,8 @@ import LoginActions from '../Redux/LoginRedux'
 
 export function* login(api, action) {
   const { username, password } = action
-  // make the call to the api
   const response = yield call(api.login, username, password)
 
-  console.log({ response })
-  // alert(JSON.stringify(response))
   if (response.ok) {
     let { data: { user, token } } = response
     yield put(LoginActions.loginSuccess(user, token))
